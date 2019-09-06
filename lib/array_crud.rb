fed6ede74331c3fd82a04e52a195fe1ce63a4bee
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-06 22:23:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.push
+  remove_element_from_end_of_array.push (new)
+  p remove_element_from_end_of_array
 end
 
 def remove_element_from_start_of_array(array)
